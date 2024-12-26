225cc97c7d02f285323c02a56cead31031ed4f13
vlad-peresta <vladyslav.peresta@ffflabel.com>
2024-12-26 16:29:04 +0200

    add endpoint for todo record updating

--- src/controllers/todo-controllers.go
+++ src/controllers/todo-controllers.go
@@ -6,6 +6,7 @@ import (
 	config "github.com/Vlad-Peresta/todo_list_go/src/conf"
 	"github.com/Vlad-Peresta/todo_list_go/src/models"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/cast"
 	"gorm.io/gorm"
 )
 
@@ -72,4 +73,48 @@ func GetAllTodos(context *gin.Context) {
 			"message": "Success",
 			"data":    todos,
 		})
+}
+
+func UpdateTodo(context *gin.Context) {
+	var data todoRequest
+
+	// Defining HTTP request parameter to get Todo id
+	reqId := context.Param("id")
+	todoId := cast.ToUint(reqId)
+
+	// Binding HTTP request body to the todoRequest struct
+	if err := context.BindJSON(&data); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Initiate empty Todo model's record
+	todo := models.Todo{}
+
+	// Get first Todo record by id from DB
+	todoById := db.Where("id = ?", todoId).First(&todo)
+	if todoById.Error != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Todo not found"})
+		return
+	}
+
+	// Matching todoRequest with models.Todo
+	todo.Name = data.Name
+	todo.Description = data.Description
+
+	// Update existing Todo record
+	result := db.Save(&todo)
+	if err := result.Error; err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	// Matching result to todoResponse
+	var response todoResponse
+	response.ID = todo.ID
+	response.Name = todo.Name
+	response.Description = todo.Description
+
+  // Creating HTTP response
+	context.JSON(http.StatusCreated, response)
 }
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,10 +1,9 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/Vlad-Peresta/todo_list_go/src/controllers"
+	"github.com/gin-gonic/gin"
 )
-
 
 // Routes function to serve endpoints
 func Routes() {
@@ -12,7 +11,7 @@ func Routes() {
 
 	route.POST("/todo", controllers.CreateTodo)
 	route.GET("/todo", controllers.GetAllTodos)
-	// route.PUT("/todo/:idTodo", controllers.UpdateTodo)
+	route.PUT("/todo/:id", controllers.UpdateTodo)
 	// route.DELETE("todo/:idTodo", controllers.DeleteTodo)
 
 	// Run route whenever triggered
