52d4c1a554e89779786cdc540b0cdd119883d049
vlad-peresta <vladyslav.peresta@ffflabel.com>
2024-12-27 17:57:13 +0200

    [IMP] add status and tag models

--- src/models/todo.go
+++ src/models/todo.go
@@ -1,12 +1,32 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
-// Define Todo table for database communications
+// Define Status database table
+type Status struct {
+	gorm.Model
+	Name  string
+	Todos []Todo
+}
+
+// Define Tag database table
+type Tag struct {
+	gorm.Model
+	Name  string
+	Todos []*Todo `gorm:"many2many:todo_tags"`
+}
+
+// Define Todo database table
 type Todo struct {
 	gorm.Model
 	Name        string
 	Description string
+	Deadline    time.Time
+	Active      bool   `gorm:"default:true"`
+	Tags        []*Tag `gorm:"many2many:todo_tags"`
+	StatusID    uint
 }
